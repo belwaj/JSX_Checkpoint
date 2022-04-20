71b10c2553dfc94f60e1f4256770d9f3b9aecc62
Wajih Belhadj <bel.wajih@gmail.com>
2022-04-20 23:36:13 +0100

    Second commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   <br />
   <img src="/imageInPublic.jpg" alt="jsx-a11y/alt-text"/>
   </div>
-  <video width="320" height="240" controls>
+  <video width="360" height="240" controls>
   <source src="myVideo.mp4" type="video/mp4" />
   </video>
  </>
